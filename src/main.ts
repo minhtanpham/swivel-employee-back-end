da486fe290d33e76d9c28f3b4e0bd6e102d6b570
Tan Pham <pmtandhqn@gmail.com>
2022-07-16 20:22:41 +0700

    install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Swivel api docs')
+    .setDescription('The api docs for Swivel demo application')
+    .setVersion('1.0')
+    .addTag('Swivel apis docs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
